aca698f5e7bf5038b1df9e58d8c4de7b344ce53a
AleT-23 <00128925@uca.edu.sv>
2025-08-20 12:28:58 -0600

    ✨ feat:Añadir funcion de solicitud de datos en archivo clase1PED.cc

--- clase1PED.cc
+++ clase1PED.cc
@@ -1,48 +1,71 @@
 #include <iostream>
 #include <string>
-#include <cstdlib>  
-#include <ctime>    
+#include <cstdlib>
+#include <ctime>
 
 // Declaración
 std::string cupones_de_premio(std::string Letras);
-void AsignarPremio (std:: string identificador_cupon);
+void AsignarPremio(std::string cupon);
+std::string SolicitarDatos();
 
-int main() 
+int main()
 {
     srand(time(0));
+    int cantidad;
 
-    std::string letras;
-    std::cout << "Ingrese las letras para su cupon: \n";
-    std::cin >> letras;
-
-    std::string cupon = cupones_de_premio(letras);
-
-    std::cout << "Su cupon generado es: " << cupon << "\n";
-    return 0;
+    std::cout << "Ingresa la cantidad de cupones que deseas generar\n";
+    std::cin >> cantidad;
+    std::string cupones[cantidad];
+    for (int i = 0; i < cantidad; i++)
+    {
+        std::string letras = SolicitarDatos();
+        std::string cupon = cupones_de_premio(letras);
+        std::cout << "Su cupon generado es: " << cupon << "\n";
+        cupones[i] = cupon;  
+        AsignarPremio(cupon);
+    }
+    for (int i = 0; i < cantidad; i++)
+    {
+        std::cout << "Cupon #" << i+1 << ": " << cupones[i] << "\n";
+    }
 }
 
 // Definición
-std::string cupones_de_premio(std::string Letras) 
+std::string cupones_de_premio(std::string Letras)
 {
-
-    int numero_aleatorio = 100 + rand() % 900;
+    int numero_aleatorio = 100 + rand() % 999;
     return Letras + std::to_string(numero_aleatorio);
 }
 
-void AsignarPremio (std:: string cupon)
+void AsignarPremio(std::string cupon)
 {
-    identificador_cupon = identificador_cupon.substr(3,4);
-    
-    int num = std::stoi(identificador_cupon);
+    std::string numeros = cupon.substr(3, 4);
+    int num = std::stoi(numeros);
 
     if (num % 2 == 0)
     {
-        std::cout <<"Es un numero entero par. Por lo tanto tiene premio. \n";
+        std::cout << "Es un numero par. Por lo tanto, tiene premio.\n";
     }
     else
     {
-        std::cout <<"Es un numero entero impar. Por lo tanto no tiene premio.\n";
+        std::cout << "Es un numero impar. Por lo tanto, no tiene premio.\n";
     }
-    
-    
+}
+
+std::string SolicitarDatos()
+{
+    std::string prefijo;
+    do
+    {
+        std::cout << "Ingrese las 3 letras para su cupon: ";
+        std::cin >> prefijo;
+
+        if (prefijo.length() != 3)
+        {
+            std::cout << "Recuerde que deben ser exactamente 3 caracteres. Vuelva a intentarlo.\n";
+        }
+
+    } while (prefijo.length() != 3);
+
+    return prefijo;
 }
