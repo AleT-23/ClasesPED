248596de2b74928db3bd7bfeffe34a60b99379aa
AleT-23 <00128925@uca.edu.sv>
2025-08-25 12:41:11 -0600

    s

--- clase1PED.cc
+++ clase1PED.cc
@@ -1,54 +1,79 @@
 #include <iostream>
+#include <vector>
 #include <string>
 #include <cstdlib>
 #include <ctime>
+#include <utility>
+
+struct InformacionCupon
+{
+    int cant_cupones;
+    std::string nombre_persona;
+    std::vector<std::pair<std::string, std::string>> cupones_generados; 
+   
+};
 
 // Declaración
 std::string cupones_de_premio(std::string Letras);
-void AsignarPremio(std::string cupon);
+std::string AsignarPremio(std::string cupon);
 std::string SolicitarDatos();
 
 int main()
 {
     srand(time(0));
-    int cantidad;
 
-    std::cout << "Ingresa la cantidad de cupones que deseas generar\n";
-    std::cin >> cantidad;
-    std::string cupones[cantidad];
-    for (int i = 0; i < cantidad; i++)
+    InformacionCupon cupon;
+
+    std::cout << "Ingresa el nombre de la persona: ";
+    std::cin >> cupon.nombre_persona;
+
+    std::cout << "Ingresa la cantidad de cupones que deseas generar: ";
+    std::cin >> cupon.cant_cupones;
+
+    for (int i = 0; i < cupon.cant_cupones; i++)
     {
+        std::cout << "\n--- Generando cupon #" << (i+1) << " ---\n";
         std::string letras = SolicitarDatos();
-        std::string cupon = cupones_de_premio(letras);
-        std::cout << "Su cupon generado es: " << cupon << "\n";
-        cupones[i] = cupon;  
-        AsignarPremio(cupon);
+        std::string cupon_generado = cupones_de_premio(letras);
+        std::string premio = AsignarPremio(cupon_generado);
+
+        
+        cupon.cupones_generados.push_back({cupon_generado, premio});
     }
-    for (int i = 0; i < cantidad; i++)
+
+    std::cout << "\n=== Lista de Cupones de " << cupon.nombre_persona << " ===\n";
+    for (int i = 0; i < cupon.cant_cupones; i++)
     {
-        std::cout << "Cupon #" << i+1 << ": " << cupones[i] << "\n";
+        std::cout << "Cupon #" << (i+1) << ": " 
+                  << cupon.cupones_generados[i].first 
+                  << " -> " << cupon.cupones_generados[i].second 
+                  << "\n";
     }
+
+    return 0;
 }
 
 // Definición
 std::string cupones_de_premio(std::string Letras)
 {
-    int numero_aleatorio = 100 + rand() % 999;
+    int numero_aleatorio = 100 + rand() % 900; 
     return Letras + std::to_string(numero_aleatorio);
 }
 
-void AsignarPremio(std::string cupon)
+std::string AsignarPremio(std::string cupon)
 {
-    std::string numeros = cupon.substr(3, 4);
+    std::string numeros = cupon.substr(3, 3); 
     int num = std::stoi(numeros);
 
     if (num % 2 == 0)
     {
         std::cout << "Es un numero par. Por lo tanto, tiene premio.\n";
+        return "Premio";
     }
     else
     {
         std::cout << "Es un numero impar. Por lo tanto, no tiene premio.\n";
+        return "Sin premio";
     }
 }
 
