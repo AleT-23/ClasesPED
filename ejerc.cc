c1545633d836a829bac6566c8c7e54b779796b63
AleT-23 <00128925@uca.edu.sv>
2025-10-22 12:21:07 -0600

    Feat: Implementacion de todo a terminar

--- ejerc.cc
+++ ejerc.cc
@@ -2,19 +2,19 @@
 #include <vector>
 #include <string>
 #include <algorithm>
-
 struct Luchador {
     std::string nombre;
     float peso;
 };
-
 Luchador ingresar();
 void ordenarPorPeso(std::vector<Luchador> &luchadores);
 void imprimirMasPesado(const std::vector<Luchador> &luchadores);
-
+bool CompararPorPeso(const Luchador &a, const Luchador &b);
+void BuscarPorNombre(std::vector<Luchador>, std:: string );
 int main() {
     std::vector<Luchador> luchadores;
     int n;
+    float peso_buscado;
     std::cout << "¿Cuántos luchadores deseas ingresar? ";
     std::cin >> n;
     for (int i = 0; i < n; i++) {
@@ -23,9 +23,16 @@ int main() {
     }
     ordenarPorPeso(luchadores);
     imprimirMasPesado(luchadores);
+    std::cout << "Ingrese el peso a buscar en lbs: ";
+    std::cin >> peso_buscado;
+    std::sort(luchadores.begin(), luchadores.end(), CompararPorPeso);
+    bool encontrado = std::binary_search(luchadores.begin(),luchadores.end(),Luchador{"", peso_buscado},CompararPorPeso);
+    if (encontrado)
+        std::cout << "Se encontró un luchador con ese peso.\n";
+    else
+        std::cout << "No se encontró ningún luchador con ese peso.\n";
     return 0;
 }
-
 Luchador ingresar() {
     Luchador peleador;
     std::cout << "Nombre del luchador: ";
@@ -34,13 +41,14 @@ Luchador ingresar() {
     std::cin >> peleador.peso;
     return peleador;
 }
-
+bool CompararPorPeso(const Luchador &a, const Luchador &b) {
+    return a.peso < b.peso;
+}
 void ordenarPorPeso(std::vector<Luchador> &luchadores) {
     std::sort(luchadores.begin(), luchadores.end(), [](const Luchador &a, const Luchador &b) {
         return a.peso > b.peso;
     });
 }
-
 void imprimirMasPesado(const std::vector<Luchador> &luchadores) {
     if (!luchadores.empty()) {
         const Luchador &masPesado = luchadores[0];
@@ -48,4 +56,13 @@ void imprimirMasPesado(const std::vector<Luchador> &luchadores) {
     } else {
         std::cout << "No hay luchadores registrados.\n";
     }
+}
+
+if ( != Luchador.end())
+{
+    std:: cout <<"Luchador encontrado:" << it-> nombre << "Peso del luchador buscado: "<<  it.peso <<"\n";
+}
+else
+{
+    std:: cout <<"Luchador no encontrado\n";
 }
